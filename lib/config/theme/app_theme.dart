96b2c3ad85975041eddad40a5644657a56f56fe7
East-Sound <kareemhekal19@gmail.com>
2025-03-22 01:31:49 +0200

    init

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,37 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-
-class AppTheme{
+class AppTheme {
   static ThemeData lightTheme = ThemeData(
       appBarTheme: AppBarTheme(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          scrolledUnderElevation: 0
-      ),
-
-
-
+          scrolledUnderElevation: 0),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
         backgroundColor: Colors.black,
-        selectedItemColor:Colors.black,
+        selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black,
-        showSelectedLabels: true, // Show text label when selected
-        showUnselectedLabels: true, // Show text label when unselected
+        showSelectedLabels: true,
+        // Show text label when selected
+        showUnselectedLabels: true,
+        // Show text label when unselected
         type: BottomNavigationBarType.fixed, // Ensures all items stay visible
       ),
-
-
       textTheme: TextTheme(
-
         bodySmall: TextStyle(
-          fontWeight:FontWeight.w400,
+          fontWeight: FontWeight.w400,
           fontSize: 14.sp,
           color: Colors.black,
         ),
-
-
-      )
-  );
+      ));
 }
--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -3,6 +3,6 @@ class Constant {
   static const String regExValidateEmail =
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
   static RegExp regexPass = RegExp(
-    r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+    r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$',
   );
 }
