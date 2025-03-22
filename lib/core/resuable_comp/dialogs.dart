2c2a428b61cadf9e97a1ef28d7fc2ee826dd54cc
اسمك <ahmedbasha01208@gmail.com>
2025-03-22 14:21:23 +0200

    feat(build):add running in web (chrome)

---
+++ lib/core/resuable_comp/dialogs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dialogs {
+  static guestDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(),
+    );
+  }
+}
