8f018ea62d69f466326b00d639b75feef03202b2
kareemHekal <kareemhekal19@gmail.com>
2025-04-02 19:19:57 +0200

    fix the problem of back arrow on the start of the app
    fix the problem of back arrow on the start of the app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
 
-          home: MainScreen(),
+         
           // routes: {
           //   RouteManager.homeScreen: (context) => HomeScreen(),
           //   RouteManager.cartScreen: (context) => CartScreen(),
